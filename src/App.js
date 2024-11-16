29ddcb4e5140ceaae418599a2b47f55c3574120e
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-11-16 21:24:38 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from '../../frontend-clean/src/components/Navbar';
+import Dashboard from '../../frontend-clean/src/components/Dashboard';
+import AddWorkout from '../../frontend-clean/src/components/AddWorkout';
+import WorkoutDetails from '../../frontend-clean/src/components/WorkoutDetails';
 import './App.css';
+import EditWorkout from '../../frontend-clean/src/components/EditWorkout';
 
 function App() {
+  const [workouts, setWorkouts] = useState([]);
+
+  const addNewWorkout = (newWorkout) => {
+    setWorkouts([...workouts, newWorkout]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add" element={<AddWorkout addNewWorkout={addNewWorkout} />} />
+          <Route path="/workout/:id" element={<WorkoutDetails />} />
+          <Route path="/edit/:id" element={<EditWorkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App
+
+console.log("it is working")
